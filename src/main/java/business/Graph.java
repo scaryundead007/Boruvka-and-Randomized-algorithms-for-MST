8bc99c3fd6a690e9cc1ba9f2370b7e92823f70d0
lucas <soumille.lucas@etu.unice.fr>
2016-04-12 14:23:06 +0200

    graph

---
+++ src/main/java/business/Graph.java
@@ -0,0 +1,104 @@
+package business;
+
+import utils.Randomizer;
+
+import java.util.*;
+
+/**
+ * Created by lucas on 12/04/16.
+ */
+public class Graph {
+
+    private int V; //nb sommet
+    private int E; //nb arete
+    private Map<Integer, List<Edge>> allVerticesWithEdges; //ensemble des arretes du graphe en fonction du sommet
+    private List<Edge> allEdges;
+
+    /**
+     * Creation des sommets (aucune arete)
+     * @param v
+     */
+    public Graph(int v) {
+        V = v;
+        this.E = 0;
+        this.allVerticesWithEdges = new HashMap<>();
+        this.allEdges = new ArrayList<>();
+    }
+
+    public Graph(int v, int e) {
+        V = v;
+        E = e;
+        for(int i = 0 ; i < e ; ++i){
+            int[] verticesId = Randomizer.getDiffRandoms(0,v);
+            int weight = Randomizer.getWeight();
+            Edge newEdge = new Edge(verticesId[0], verticesId[1], weight);
+            addEdge(newEdge);
+        }
+    }
+
+    public int getV() {
+        return V;
+    }
+
+    public int getE() {
+        return E;
+    }
+
+    public void addEdge(Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        if(checkVertex(v) && checkVertex(w)){
+            //si le sommet contient deja une arrete on met a jour la liste
+            if(allVerticesWithEdges.get(new Integer(v)) != null)
+                allVerticesWithEdges.get(new Integer(v)).add(e);
+            //sinon on crée une nouvelle entrée dans la map
+            else {
+                List<Edge> edges = new ArrayList<>();
+                edges.add(e);
+                allVerticesWithEdges.put(new Integer(v), edges);
+            }
+            if(allVerticesWithEdges.get(new Integer(w)) != null)
+                allVerticesWithEdges.get(new Integer(w)).add(e);
+                //sinon on crée une nouvelle entrée dans la map
+            else {
+                List<Edge> edges = new ArrayList<>();
+                edges.add(e);
+                allVerticesWithEdges.put(new Integer(w), edges);
+            }
+            allEdges.add(e);
+            E++;
+        }
+    }
+
+    /**
+     * return the edges connected to the vertice
+     * @param vertice
+     * @return
+     */
+    public Iterable<Edge> getAdjacent(int vertice){
+        return allVerticesWithEdges.get(new Integer(vertice));
+    }
+
+    public Iterable<Edge> getAllEdges() {
+        return allEdges;
+    }
+
+    /**
+     * check if the vertex is in the graph
+     * @param vertex
+     * @return
+     */
+    private boolean checkVertex(int vertex){
+        return (vertex > 0 && vertex < V);
+    }
+
+    @Override
+    public String toString() {
+        String intro = "Graph{" +
+                "V=" + V +
+                ", E=" + E +
+                '}';
+        //faire l'affichage
+        return intro;
+    }
+}
